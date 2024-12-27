d88a36185e3ecc03bea3446950c344ca7573d6b5
Gregadeaux <gregadeaux@gmail.com>
2024-12-27 15:08:52 -0600

    Added defaults for Gains and CanDef

--- src/main/java/frc/robot/util/CanDef.java
+++ src/main/java/frc/robot/util/CanDef.java
@@ -1,59 +1,59 @@
 package frc.robot.util;
 
 public class CanDef {
-  private final int id;
-  private final CanBus bus;
+    private final int id;
+    private final CanBus bus;
 
-  public enum CanBus {
-    Rio("RIO"),
-    CANivore("CANivore");
+    public enum CanBus {
+        Rio("RIO"),
+        CANivore("CANivore");
 
-    private final String name;
+        private final String name;
 
-    private CanBus(String name) {
-      this.name = name;
-    }
-  }
-
-  private CanDef(int id, CanBus bus) {
-    if (id < 0) {
-      throw new IllegalArgumentException(
-          "CAN ID (" + id + ", " + bus.name + ") must be non-negative");
+        private CanBus(String name) {
+            this.name = name;
+        }
     }
 
-    this.id = id;
-    this.bus = bus;
-  }
+    private CanDef(int id, CanBus bus) {
+        if (id < 0) {
+            throw new IllegalArgumentException(
+                    "CAN ID (" + id + ", " + bus.name + ") must be non-negative");
+        }
 
-  public int id() {
-    return id;
-  }
-
-  public String bus() {
-    return bus.name;
-  }
-
-  public static Builder builder() {
-    return new Builder();
-  }
-
-  // create a builder for this class
-  public static class Builder {
-    private int id;
-    private CanBus bus;
-
-    public Builder id(int id) {
-      this.id = id;
-      return this;
+        this.id = id;
+        this.bus = bus;
     }
 
-    public Builder bus(CanBus bus) {
-      this.bus = bus;
-      return this;
+    public int id() {
+        return id;
     }
 
-    public CanDef build() {
-      return new CanDef(id, bus);
+    public String bus() {
+        return bus.name;
     }
-  }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    // create a builder for this class
+    public static class Builder {
+        private int id = -1;
+        private CanBus bus = CanBus.Rio;
+
+        public Builder id(int id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder bus(CanBus bus) {
+            this.bus = bus;
+            return this;
+        }
+
+        public CanDef build() {
+            return new CanDef(id, bus);
+        }
+    }
 }
