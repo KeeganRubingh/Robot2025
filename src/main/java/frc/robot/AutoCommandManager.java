81a4b5d67c042bf0f1b3bbade07e2555ebc87bf5
silasnerad <silasnerad@gmail.com>
2025-02-13 19:24:53 -0600

    Auto changes and created a new one

--- src/main/java/frc/robot/AutoCommandManager.java
+++ src/main/java/frc/robot/AutoCommandManager.java
@@ -29,6 +29,8 @@ public class AutoCommandManager {
     PathPlannerAuto Straight = new PathPlannerAuto("Straight");
     PathPlannerAuto Diagonal = new PathPlannerAuto("Diagonal");
     PathPlannerAuto simpleAuto = new PathPlannerAuto("simpleAuto");
+    PathPlannerAuto advancedPath = new PathPlannerAuto("advancedPath");
+    PathPlannerAuto exitZone = new PathPlannerAuto("exitZone");
 
     // Set up SysId routines
     autoChooser.addOption(
@@ -49,6 +51,9 @@ public class AutoCommandManager {
     autoChooser.addOption("Straight", Straight);
     autoChooser.addOption("Diagonal", Diagonal);
     autoChooser.addOption("simpleAuto", simpleAuto);
+    autoChooser.addOption("advancedPath", advancedPath);
+    autoChooser.addOption("exitZone", exitZone);
+
 
     // Add Choreo Paths
     autoChooser.addOption("Characterization Test Path", CharacterizationTest);
