fb5a69c2d5fa879e425c24407e3303d0afbc6212
silasnerad <silasnerad@gmail.com>
2025-02-22 10:54:26 -0600

    Fixed the major rebase issue Brought deploy from autoTesting Added advancedPathPlanner and events

--- src/main/java/frc/robot/AutoCommandManager.java
+++ src/main/java/frc/robot/AutoCommandManager.java
@@ -64,7 +64,7 @@ public class AutoCommandManager {
   }
 
   private void configureNamedCommands(Drive drive, ArmJoint shoulder, ArmJoint elbow, Elevator elevator, Wrist wrist, CoralEndEffector coralEE, AlgaeEndEffector algaeEE) {
-      NamedCommands.registerCommand("StopIntake", new StowCommand(shoulder, elbow, elevator, wrist, coralEE, algaeEE));
+      NamedCommands.registerCommand("Stow", new StowCommand(shoulder, elbow, elevator, wrist, coralEE, algaeEE));
       NamedCommands.registerCommand("StartIntake", new TakeCoral(shoulder, elbow, elevator, wrist, coralEE));
       NamedCommands.registerCommand("L4Score", new StowToL4(shoulder, elbow, elevator, wrist, coralEE));
       NamedCommands.registerCommand("L1Score", new StowToL1(shoulder, elbow, wrist, coralEE));
