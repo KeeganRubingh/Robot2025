8e3c56d9588d981bcc2892bbf578b8bec86d43de
swegner <steve.wegner@gmail.com>
2025-02-24 20:36:49 -0600

    Adjust simpleAutoPlanner paths - Switch to NamedCommands: StartFrontIntake -> StationIntake

--- src/main/java/frc/robot/AutoCommandManager.java
+++ src/main/java/frc/robot/AutoCommandManager.java
@@ -66,7 +66,7 @@ public class AutoCommandManager {
 
   private void configureNamedCommands(Drive drive, ArmJoint shoulder, ArmJoint elbow, Elevator elevator, Wrist wrist, CoralEndEffector coralEE, AlgaeEndEffector algaeEE) {
       NamedCommands.registerCommand("Stow", new StowCommand(shoulder, elbow, elevator, wrist, coralEE, algaeEE));
-      NamedCommands.registerCommand("StartFrontIntake", new StationIntakeCommand(shoulder, elbow, elevator, wrist, coralEE));
+      NamedCommands.registerCommand("StationIntake", new StationIntakeCommand(shoulder, elbow, elevator, wrist, coralEE));
       NamedCommands.registerCommand("StartIntake", new StationIntakeReverseCommand(shoulder, elbow, elevator, wrist, coralEE));
       NamedCommands.registerCommand("L4Score", new StowToL4(shoulder, elbow, elevator, wrist));
       NamedCommands.registerCommand("L1Score", new StowToL1(shoulder, elbow, wrist));
