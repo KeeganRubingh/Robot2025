92fce886d0f8ff1545ae5f12d4f5123dddf43753
theomatter <theomatter2@gmail.com>
2025-02-25 20:30:41 -0600

    Adjusted to climber voltage values from TunerX test

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -454,7 +454,7 @@ public class RobotContainer {
       .onTrue(
         climber.getNewSetServoAngleCommand(0.0)
         .andThen(new WaitCommand(0.1))
-        .andThen(climber.getNewSetVoltsCommand(6.0))
+        .andThen(climber.getNewSetVoltsCommand(-3.0)) // negative was forward in test
       )
       .onFalse(
         climber.getNewSetVoltsCommand(0.0)
@@ -466,7 +466,7 @@ public class RobotContainer {
     .onTrue(
       climber.getNewSetServoAngleCommand(0.0)
       .andThen(new WaitCommand(0.1))
-      .andThen(climber.getNewSetVoltsCommand(-6.0))
+      .andThen(climber.getNewSetVoltsCommand(3.0))
     )
     .onFalse(
       climber.getNewSetVoltsCommand(0.0)
