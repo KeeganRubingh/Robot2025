f5afc7a63273a911a4db1f170af29d8752a73e2e
silasnerad <silasnerad@gmail.com>
2025-03-03 18:01:15 -0600

    Fix auto changes and update vendor libraries

--- src/main/java/frc/robot/commands/StowCommand.java
+++ src/main/java/frc/robot/commands/StowCommand.java
@@ -21,7 +21,7 @@ public class StowCommand extends SequentialCommandGroup {
 
     private enum ShoulderPositions {
         Starting(new LoggedTunableNumber("StowCommand/shoulder/StartingDegrees", 0)),
-        SafeToSwingElbow(new LoggedTunableNumber("StowCommand/shoulder/SafeToSwingElbowDegrees", 75)),
+        SafeToSwingElbow(new LoggedTunableNumber("StowCommand/shoulder/SafeToSwingElbowDegrees", 105)),
         SafeToSwingElbowHigh(new LoggedTunableNumber("StowCommand/shoulder/SafeToSwingElbowHighDegrees", 10)),
         Final(new LoggedTunableNumber("StowCommand/shoulder/FinalDegrees", 90));
 
