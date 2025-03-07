945456866d367b5966eb3769ff8e68fb45884a88
silasnerad <silasnerad@gmail.com>
2025-03-06 18:00:14 -0600

    Comment out characterization and moved test autos into a new folder to prevent them showing up in chooser and pathplanner

--- src/main/java/frc/robot/AutoCommandManager.java
+++ src/main/java/frc/robot/AutoCommandManager.java
@@ -38,10 +38,34 @@ public class AutoCommandManager {
 
   public AutoCommandManager(Drive drive, ArmJoint shoulder, ArmJoint elbow, Elevator elevator, Wrist wrist, CoralEndEffector coralEE, AlgaeEndEffector algaeEE) {
     configureNamedCommands(drive, shoulder, elbow, elevator, wrist, coralEE, algaeEE);;
-
-    // Set up auto routines
-    autoChooser = new LoggedDashboardChooser<>("Auto Choices", AutoBuilder.buildAutoChooser());
     
+
+    // PathPlannerAuto CharacterizationTest = new PathPlannerAuto("CharacterizeAuto");
+    // PathPlannerAuto ChoreoStraightAuto = new PathPlannerAuto("ChoreoStraightAuto");
+
+     // Set up auto routines
+     autoChooser = new LoggedDashboardChooser<>("Auto Choices", AutoBuilder.buildAutoChooser());
+
+    // // Set up SysId routines
+    // autoChooser.addOption(
+    //     "Drive Wheel Radius Characterization", DriveCommands.wheelRadiusCharacterization(drive));
+    // autoChooser.addOption(
+    //     "Drive Simple FF Characterization", DriveCommands.feedforwardCharacterization(drive));
+    // autoChooser.addOption(
+    //     "Drive SysId (Quasistatic Forward)",
+    //     drive.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
+    // autoChooser.addOption(
+    //     "Drive SysId (Quasistatic Reverse)",
+    //     drive.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
+    // autoChooser.addOption(
+    //     "Drive SysId (Dynamic Forward)", drive.sysIdDynamic(SysIdRoutine.Direction.kForward));
+    // autoChooser.addOption(
+    //     "Drive SysId (Dynamic Reverse)", drive.sysIdDynamic(SysIdRoutine.Direction.kReverse));
+
+
+    // // Add Choreo Paths
+    // autoChooser.addOption("Characterization Test Path", CharacterizationTest);
+    // autoChooser.addOption("[Choreo] Straight Test", ChoreoStraightAuto);
   }
 
   public Command getAutonomousCommand() {
