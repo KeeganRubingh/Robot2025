d5e97ad924317159d5c9ab588e0c23f79b5dc255
Liam-Rohrer <powercat201@icloud.com>
2025-03-08 16:57:01 -0600

    Tune Paths in Real Life Tweaked AdvancedPathPlan for more accuracy Changed AdvancedPathPlan timing Changed Robot Bumper Length Set L4 named command to L3 for testing Changed timings for StowToL3

--- src/main/java/frc/robot/AutoCommandManager.java
+++ src/main/java/frc/robot/AutoCommandManager.java
@@ -50,25 +50,25 @@ public class AutoCommandManager {
       NamedCommands.registerCommand("StowToL1", new StowToL1(shoulder, elbow, wrist));
       NamedCommands.registerCommand("StowToL2", new StowToL2(shoulder, elbow, elevator, wrist));
       NamedCommands.registerCommand("StowToL3", new StowToL3(shoulder, elbow, wrist, elevator));
-      NamedCommands.registerCommand("StowToL4", new StowToL4(shoulder, elbow, elevator, wrist));
+      NamedCommands.registerCommand("StowToL4", new StowToL3(shoulder, elbow, wrist, elevator));
       NamedCommands.registerCommand("ScoreL1",StowToL1.getNewScoreCommand(coralEE)
-        .andThen(new WaitCommand(0.02))
+        .andThen(new WaitCommand(0.2))
         .andThen(StowToL1.getNewStopScoreCommand(coralEE)));
       NamedCommands.registerCommand("ScoreL2",StowToL2.getNewScoreCommand(elbow, wrist, coralEE)
-        .andThen(new WaitCommand(0.02))
+        .andThen(new WaitCommand(0.2))
         .andThen(StowToL2.getNewStopScoreCommand(elbow, wrist, coralEE)));
       NamedCommands.registerCommand("ScoreL3",StowToL3.getNewScoreCommand(elbow, wrist, coralEE)
-        .andThen(new WaitCommand(0.02))
+        .andThen(new WaitCommand(0.2))
         .andThen(StowToL3.getNewStopScoreCommand(elbow, wrist, coralEE)));
-      NamedCommands.registerCommand("ScoreL4",StowToL4.getNewScoreCommand(elbow, wrist, coralEE)
-        .andThen(new WaitCommand(0.02))
-        .andThen(StowToL4.getNewStopScoreCommand(elbow, wrist, coralEE)));
+      NamedCommands.registerCommand("ScoreL4",StowToL3.getNewScoreCommand(elbow, wrist, coralEE)
+        .andThen(new WaitCommand(0.2))
+        .andThen(StowToL3.getNewStopScoreCommand(elbow, wrist, coralEE)));
 
       // MAY NOT use added Score Command
       NamedCommands.registerCommand("StopScoreL1",StowToL1.getNewStopScoreCommand(coralEE));
       NamedCommands.registerCommand("StopScoreL2",StowToL2.getNewStopScoreCommand(elbow, wrist, coralEE));
       NamedCommands.registerCommand("StopScoreL3",StowToL3.getNewStopScoreCommand(elbow, wrist, coralEE));
-      NamedCommands.registerCommand("StopScoreL4",StowToL4.getNewStopScoreCommand(elbow, wrist, coralEE));
+      NamedCommands.registerCommand("StopScoreL4",StowToL3.getNewStopScoreCommand(elbow, wrist, coralEE));
   
       NamedCommands.registerCommand("CoralOuttake", new OutakeCoral(coralEE));
       NamedCommands.registerCommand("StopDrivetrain", new StopDrivetrainCommand(drive));
