c038dced409562cc8ee28031a5de90d151f7883c
Jack Schumacher <84467112+jackrschumacher@users.noreply.github.com>
2025-03-12 18:51:38 -0500

    ReZero'd Wrist

--- src/main/java/frc/robot/subsystems/wrist/WristIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/wrist/WristIOTalonFX.java
@@ -26,7 +26,7 @@ public class WristIOTalonFX implements WristIO {
   public CoastOut coastRequest;
   public TalonFX Motor;
   public CANcoder canCoder;
-  public Angle canCoderOffset = Degrees.of(-144.13 + 16);
+  public Angle canCoderOffset = Degrees.of(-144.13 + 16 + 12);
   private Angle m_setPoint = Angle.ofRelativeUnits(0, Rotations);
 
   public WristIOTalonFX(CanDef canbus,CanDef canCoderDef) {
