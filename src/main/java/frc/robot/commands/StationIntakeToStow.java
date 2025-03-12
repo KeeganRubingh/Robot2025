c8d8f0810e07b6f553253bb4142e4a83db79487a
silasnerad <silasnerad@gmail.com>
2025-03-11 20:40:36 -0500

    Implemented Station Auto Align

--- src/main/java/frc/robot/commands/StationIntakeToStow.java
+++ src/main/java/frc/robot/commands/StationIntakeToStow.java
@@ -37,8 +37,28 @@ public class StationIntakeToStow extends SequentialCommandGroup {
         }
     }
 
+    private enum ShoulderPositions {
+        SafeToTwistWrist(new LoggedTunableNumber("StationIntakeToStow/shoulder/SafeToTwistWrist", 105)),
+        MidPoint(new LoggedTunableNumber("StationIntakeToStow/shoulder/MidPoint", 100));
+
+        DoubleSupplier position;
+        MutAngle distance;
+
+        ShoulderPositions(DoubleSupplier position) {
+            this.position = position;
+            this.distance = Degrees.mutable(0.0);
+        }
+
+        public Angle angle() {
+            this.distance.mut_replace(this.position.getAsDouble(), Degrees);
+            return this.distance;
+        }
+    }
+
     public StationIntakeToStow(ArmJoint shoulder, ArmJoint elbow, Elevator elevator, Wrist wrist, CoralEndEffector coralEE, AlgaeEndEffector algaeEE) {
         super(
+            shoulder.getNewSetAngleCommand(ShoulderPositions.MidPoint.position),
+            new WaitUntilCommand(shoulder.getNewLessThanAngleTrigger(ShoulderPositions.SafeToTwistWrist.position)),
             wrist.getNewWristTurnCommand(WristPositions.Final.position),
             // Needed so coral doesn't hit bar on way back
             new WaitUntilCommand(wrist.getNewGreaterThanAngleTrigger(WristPositions.SafeToSwingShoulder.position)),
