fed56c2c7a1990ef3e5d2c5adac40780d1639331
swegner <steve.wegner@gmail.com>
2025-03-17 18:43:29 -0500

    Fixed Positioning of Brake in AutoAlign to not break backup

--- src/main/java/frc/robot/commands/ReefScoreCommandFactory.java
+++ src/main/java/frc/robot/commands/ReefScoreCommandFactory.java
@@ -156,7 +156,8 @@ public class ReefScoreCommandFactory {
     public static Command getNewReefCoralScoreSequence(ReefPosition position, boolean moveArm, boolean isBackingUp, Map<ReefPositionsUtil.ScoreLevel,Command> coralLevelCommands, Map<ReefPositionsUtil.ScoreLevel,Command> scoreCoralLevelCommands, Map<ReefPositionsUtil.ScoreLevel,Command> stopCoralLevelCommands, Drive drive) {
         return 
             getNewAlignToReefCommand(position, true, drive).onlyIf(()->isBackingUp)
-                .alongWith(DriveCommands.brakeDrive(drive).andThen(ReefPositionsUtil.getInstance().getCoralLevelSelector(coralLevelCommands)).onlyIf(() -> {return moveArm;}))
+                .andThen(DriveCommands.brakeDrive(drive).onlyIf(() -> {return moveArm;}))
+                .alongWith(ReefPositionsUtil.getInstance().getCoralLevelSelector(coralLevelCommands).onlyIf(() -> {return moveArm;}))
             .andThen(getNewAlignToReefCommand(position, false, drive))
             // .andThen(new WaitCommand(0.2))
             .andThen(ReefPositionsUtil.getInstance().getCoralLevelSelector(scoreCoralLevelCommands))
