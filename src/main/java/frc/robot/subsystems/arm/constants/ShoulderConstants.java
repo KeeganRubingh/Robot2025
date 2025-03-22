502b77f52ffbef6a9a9ebbebcd1e49669574ad75
Jack Schumacher <84467112+jackrschumacher@users.noreply.github.com>
2025-03-22 16:10:04 -0500

    Changed Arm Slot LoggedTunable Number Names

--- src/main/java/frc/robot/subsystems/arm/constants/ShoulderConstants.java
+++ src/main/java/frc/robot/subsystems/arm/constants/ShoulderConstants.java
@@ -25,8 +25,8 @@ public class ShoulderConstants extends ArmJointConstants {
             Gains.builder().kS(0.0).kG(0.0).kV(0.0).kA(0.0).kP(0.1).kI(0.0).kD(0.0).build();
 
         this.LoggedName = "Shoulder";
-        this.TalonFXGains = new LoggedTunableGainsBuilder("ArmJoint"+LoggedName+"/ShoulderSlot0Gains", 50.0, 0, 0.0, 1.0, 0.0, 6.0, 0.3, 1.0, 2.0, 0, 0, 0);
-        this.TalonFXGainsSlot1 = new LoggedTunableGainsBuilder("ArmJoint"+LoggedName+"/ShoulderSlot1Gains", 50.0, 0, 0.0, 1.0, 0.0, 6.0, 0.3, 1.0, 2.0, 0, 0, 0);
+        this.TalonFXGains = new LoggedTunableGainsBuilder("ArmJoint"+LoggedName+"/Slot0/", 50.0, 0, 0.0, 1.0, 0.0, 6.0, 0.3, 1.0, 2.0, 0, 0, 0);
+        this.TalonFXGainsSlot1 = new LoggedTunableGainsBuilder("ArmJoint"+LoggedName+"/Slot1/", 50.0, 0, 0.0, 1.0, 0.0, 6.0, 0.3, 1.0, 2.0, 0, 0, 0);
 
         this.MaxVelocity = RotationsPerSecond.of(1);
         this.MaxAcceleration = RotationsPerSecondPerSecond.of(5);
