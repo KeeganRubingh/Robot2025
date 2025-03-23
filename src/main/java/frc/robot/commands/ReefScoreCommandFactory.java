080352a5616d3b270fb5ddf6550aa42df3279996
NickTheFox99 <breakoutboy99@gmail.com>
2025-03-22 20:39:44 -0500

    Edit Triggers for Algae Score

--- src/main/java/frc/robot/commands/ReefScoreCommandFactory.java
+++ src/main/java/frc/robot/commands/ReefScoreCommandFactory.java
@@ -15,6 +15,7 @@ import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.subsystems.algaeendeffector.AlgaeEndEffector;
 import frc.robot.subsystems.arm.ArmJoint;
 import frc.robot.subsystems.drive.Drive;
@@ -196,7 +197,8 @@ public class ReefScoreCommandFactory {
                 new TakeAlgaeL2(shoulder, elbow, wrist, algaeEE, elevator),
                 new TakeAlgaeL3(shoulder, elbow, wrist, algaeEE, elevator),
                 () -> ReefPositionsUtil.getInstance().isSelected(DeAlgaeLevel.Low)))
-            .andThen(new WaitCommand(0.5))
+            .until(algaeEE.hasAlgaeTrigger())
+            .andThen(new WaitUntilCommand(algaeEE.hasAlgaeTrigger()))
             .andThen(getNewAlignToReefCommand(ReefPosition.Center, true, drive));
     }
     
